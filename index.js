7dd483f5c1591795708f7dff09a1b25334f19fdd
tugi-ngunjiri <blueqwinn@gmail.com>
2022-11-24 22:37:38 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+customerName 
+ let x = (John)
+ SetBestcustomerName 
+ let p = (Jane)
+ bestCustomer
+ let C = 'not Bob'
+ overwriteBestCustomer
+ let K =(Mary)
+ overwriteBestCustomer()
+ let T = (ruby)
+ leastFavoriteCustomer
+ let F =(Tom)
+ changeLeastFavoriteCustomer
+ let o =(Cindy)
+ changeLeastFavoriteCustomer
+ let i = (Gordon)
+ changeLeastFavoriteCustomer
+ let D =(Diana)
+
+
